b1caafe2156920bea03fe53edd0154939bf5dce7
ranokay <ranokay+github@oxystack.com>
2024-01-27 17:53:35 +0200

    Refactor code for improved performance

---
+++ src/pages/newtab/index.tsx
@@ -0,0 +1,10 @@
+import { render } from 'solid-js/web'
+import Newtab from './Newtab'
+import './index.css'
+
+const appContainer = document.querySelector('#app-container')
+if (!appContainer) {
+	throw new Error('Can not find AppContainer')
+}
+
+render(Newtab, appContainer)
---
+++ src/pages/options/index.tsx
@@ -0,0 +1,10 @@
+import { render } from 'solid-js/web'
+import Option from './Options'
+import './index.css'
+
+const appContainer = document.querySelector('#app-container')
+if (!appContainer) {
+	throw new Error('Can not find AppContainer')
+}
+
+render(Option, appContainer)
